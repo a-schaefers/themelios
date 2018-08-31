bce6f6fe83ffc9588d7f654823f20a99dd385131
Adam Schaefers <sch@efers.org>
2018-08-31 01:58:45 -0700

    add example file

---
+++ example_configuration.sh
@@ -0,0 +1,25 @@
+# Themelios configuration.sh example
+POOL_NAME="zroot"
+POOL_TYPE="raidz1"    # May also be set to "mirror". Leave empty "" for single.
+
+# use one disk per line here!
+POOL_DISKS="/dev/sda
+/dev/sdb
+/dev/sdc"
+
+SGDISK_CLEAR="true"   # Use sgdisk --clear
+WIPEFS_ALL="true"     # wipefs --all
+ZERO_DISKS="false"
+ATIME="off"           # recommended "off" for SSD disks.
+SNAPSHOT_ROOT="true"  # Sets the value of com.sun:auto-snapshot
+SNAPSHOT_HOME="true"
+USE_ZSWAP="false"     # Creates a swap zvol
+ZSWAP_SIZE=4G
+
+# Your top-level configuration.nix file relative path from the project_root.
+# e.g. for the file project_root/hosts/hpZ620/default.nix use the following:
+# TOP_LEVEL_NIXFILE="hosts/hpZ620/default.nix"
+TOP_LEVEL_NIXFILE="hosts/vm.nix"
+
+# Directory name of <git-remote> in "/" (root). Do not use slashes.
+NIXCFG_DIR="nix-config"
