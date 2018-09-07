3dc526b80ce98fd3f361ba11a91c376db150f434
Adam Schaefers <sch@efers.org>
2018-09-06 19:35:09 -0700

    just a minimal vm nix file

--- hosts/vm-example/default.nix
+++ hosts/vm-example/default.nix
@@ -1,26 +1,24 @@
 { config, pkgs, ... }:
 # zfs test vm settings
 {
-  imports = [];
-  boot.kernelModules = [ "microcode" ];
-  boot.kernelParams = [ "" ];
+imports = [];
 
-  i18n = {
-    consoleFont = "Lat2-Terminus16";
-    consoleKeyMap = "us";
-    defaultLocale = "en_US.UTF-8";
-  };
+i18n = {
+consoleFont = "Lat2-Terminus16";
+consoleKeyMap = "us";
+defaultLocale = "en_US.UTF-8";
+};
 
-  time.timeZone = "America/Los_Angeles";
+time.timeZone = "America/Los_Angeles";
 
-  programs.mtr.enable = true;
-  programs.bash.enableCompletion = true;
+programs.mtr.enable = true;
+programs.bash.enableCompletion = true;
 
-  networking.hostName = "themelios-vm";
+networking.hostName = "themelios-vm";
 
-  # This value determines the NixOS release with which your system is to be
-    # compatible, in order to avoid breaking some software such as database
-  # servers. You should change this only after NixOS release notes say you
-  # should.
-  system.stateVersion = "18.03"; # Did you read the comment?
+# This value determines the NixOS release with which your system is to be
+# compatible, in order to avoid breaking some software such as database
+# servers. You should change this only after NixOS release notes say you
+# should.
+system.stateVersion = "18.03"; # Did you read the comment?
 }
