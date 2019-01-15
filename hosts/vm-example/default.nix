3a7a1bdddba0072410e433a5908344d87b72cf66
Adam Schaefers <sch@efers.org>
2019-01-14 18:09:47 -0800

    update example state version

--- hosts/vm-example/default.nix
+++ hosts/vm-example/default.nix
@@ -16,9 +16,12 @@ programs.bash.enableCompletion = true;
 
 networking.hostName = "themelios-vm";
 
+
+# system.stateVersion (set to the same version as the ISO installer)
+
 # This value determines the NixOS release with which your system is to be
 # compatible, in order to avoid breaking some software such as database
 # servers. You should change this only after NixOS release notes say you
 # should.
-system.stateVersion = "18.03"; # Did you read the comment?
+system.stateVersion = "18.09"; # Did you read the comment?
 }
