5a49d08c50c0903772bdf86a121f298edc44a286
Adam Schaefers <paxchristi888@gmail.com>
2019-07-31 14:48:48 -0700

    2.0 complete

--- hosts/vm-example/configuration.sh
+++ hosts/vm-example/configuration.sh
@@ -6,7 +6,7 @@ use_zero_disks="false"      # use dd if=/dev/zero ...
 zfs_pool_name="rpool"
 zfs_pool_disks=("/dev/sda") # Note: using /dev/disk/by-id is also preferable.
 zfs_pool_type=""            # use "" for single, or "mirror", "raidz1", etc.
-zfs_encrypt_home="true"
+zfs_encrypt_home="false"    # only set to true if you are using a nixos ISO with ZFS 0.8 or higher.
 zfs_auto_snapshot=("$zfs_pool_name/HOME" "$zfs_pool_name/ROOT") # datasets to be set with com.sun:auto-snapshot=true
 nix_top_level_configuration="hosts/vm-example" # Your top-level nix file to be bootstrapped
 nix_zfs_configuration_extra_enabled="false" # uncomment below if set to true
